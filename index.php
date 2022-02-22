1fc2c62b984eba86786207d6ef428f6340bfc333
CeDestiny <46763137+CeDestiny@users.noreply.github.com>
2022-02-21 18:42:23 -0800

    initial commit
    search, image lazy loading

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function searchNBA(str) {
+  if (str.length == 0) {
+    document.getElementById("txtNbaResults").innerHTML = "Please enter a search result";
+    return;
+  } else {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("txtNbaResults").innerHTML = this.responseText;
+      }
+    }
+    xmlhttp.open("GET", "nbasearch.php?search="+encodeURI(str), true);
+    xmlhttp.send();
+  }
+}
+</script>
+</head>
+<body>
+
+<p><b>Start typing a name in the input field below:</b></p>
+<form action="">
+  <label for="fname">NBA Search:</label>
+  <input type="text" id="fname" name="fname" onkeyup="searchNBA(this.value)">
+</form>
+<p>Results:<br/>
+<span id="txtNbaResults"></span></p>
+
+</body>
+</html>
---
+++ nbasearch.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="/styles/nba.css" type="text/css" />
+  <script src="/js/lazysizes.min.js" async="">
+	//document.addEventListener("DOMContentLoaded", yall);
+  </script>
+<?php
+function getSearchResults() {
+	$search = filter_input(INPUT_GET, 'search', FILTER_SANITIZE_EMAIL);
+
+	if ($search === null)
+	{
+		echo "No results";
+	} else
+	{
+		echo "The search result for <mark> $search </mark>.";
+
+		$nbaHeadshotPrefix = "https://cdn.nba.com/headshots/nba/latest/260x190/";
+		$nbaHeadshotPostfix = ".png";
+
+		include 'dbsetup.php';
+		$query = "SELECT levenshtein_ratio(name, ?) as score, name, team, points, ppg, nbaid from stats order by 1 desc limit 20";
+		echo $query."<br/>";
+		$stmt = $conn->prepare($query);
+		$stmt->bind_param('s', $search);
+		//$query = "SELECT * from stats limit 20";
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		foreach ($result as $row) {
+			echo "<div class='nba_container'>\n";
+				echo "<div class='nba_headshot_container'>\n";
+					echo "<img class='lazyload' src='/images/nba_placeholder.png' data-src='".$nbaHeadshotPrefix.$row["nbaid"].$nbaHeadshotPostfix."' onerror='this.src=\"/images/nba_placeholder.png\"'>\n";
+					// onerror='this.parentNode.removeChild(this)' onload='this.parentNode.backgroundImage=none'
+				echo "</div>\n";
+				echo "<div class='nba_player_stats'>\n";
+					echo "<h2>".$row['name']."</h2>\n";
+					echo "<div class='nba_vert_stats'><b>Team: </b>".$row['team']."</div>\n";
+					echo "<div class='nba_vert_stats'><b>Points: </b>".$row['points']."</div>\n";
+					echo "<div class='nba_vert_stats'><b>PPG: </b>".$row['ppg']."</div>\n";
+				echo "</div>\n";
+			echo "</div>\n";
+		}
+	}
+}
+?>
+
+</head>
+<body>
+<?php getSearchResults() ?>
+</body>
+</html>
