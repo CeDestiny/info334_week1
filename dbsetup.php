953df7772bbd267814b055b5bbc4c07d83e482e5
CeDestiny <46763137+CeDestiny@users.noreply.github.com>
2022-02-25 18:19:36 -0800

    change db to point to rds

--- dbsetup.php
+++ dbsetup.php
@@ -1,8 +1,16 @@
 <?php
+/*
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "nbastats";
+*/
+
+$servername = "dbweek1.celdz2ekldnz.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "week1password";
+$dbname = "nbastats";
+$port = 3306;
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
